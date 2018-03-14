1d942f4d65d6a7ddab8718c6614edb9f293f74d2
DimiG <dim.georgiev1976@yandex.com>
2018-03-14 16:54:44 +0800

    файлом forms/comment.htm

--- js/main.js
+++ js/main.js
@@ -1,7 +1,49 @@
 $(document).ready(function() {
 
-	console.log('Hello world!');
+	// Validate comment form
+	(function(){
+		var formCommentValidation = {
+			init: function(){
+				this._setUpListeners();
+			},
 
-});
+			_setUpListeners: function(){
+				$('#sendComment').on('click', formCommentValidation._validateSendForm);
+			},
 
+			_validateSendForm: function(event){
+				event.preventDefault();
+				
+				var valid = true,
+					userCommentTextarea = $("#userCommennt"),
+					userComment = $("#userCommennt").val().trim(),
+					errorMessage = $("#errorMessage");
+				
+				if ( userComment.length === 0 ) {
+					errorMessage.addClass('show-error');
+					valid = false;
+				} else {
+					errorMessage.removeClass('show-error');
+				}
+							// Hide errors
+				userCommentTextarea.on('change', function(){
+					errorMessage.removeClass('show-error');
+				});  
+				userCommentTextarea.on( 'keydown ', function(){
+					errorMessage.fadeOut(800);
+				});  
 
+					if ( valid === true ) {
+					console.log('Sending form!');
+					//Send comment without whitespace
+					userCommentTextarea.val(userComment);
+					// $("#CommentForm").submit();
+					$('#CommentForm').unbind('submit').submit();
+				}
+			}
+		};
+
+		formCommentValidation.init();
+	}());
+	});
+							// End Validate comment form
