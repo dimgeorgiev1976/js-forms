581c722c2cfff7fa3ff16edfc89ee0e26ec5c8ed
DimiG <dim.georgiev1976@yandex.com>
2018-03-16 13:44:13 +0800

    #2 commit изменен js

--- js/main.js
+++ js/main.js
@@ -47,3 +47,66 @@ $(document).ready(function() {
 	}());
 	});
 							// End Validate comment form
+
+
+(function(){
+			var formLoginValidation = {
+						isValid: true,
+								init: function(){
+									// вызов внутренних функций
+									this._setUpListeners();
+								},
+						_setUpListeners: function(){
+							$('#submitLoginForm').on('click', formLoginValidation._validateForm );
+
+							   var inputs = $('#loginForm').find('input');
+							   $.each(inputs, function(index, val){
+					var input = $(val);
+					// Hide errors
+					input.on('focus keydown change', function(){
+						$(".show-error").removeClass('show-error');
+					});	
+
+		});
+
+					},
+
+_validateForm: function(event){
+event.preventDefault();
+var form = $('#loginForm'),
+    email = $('#email').val().trim(),
+    password = $("#password").val().trim(),
+					validEmail = "mail@mail.com",
+					validPassword = "123",
+					pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i,
+					valid = true;
+
+					//Check email
+				if ( email.length === 0 ) {
+					// Show errors
+					$("#emptyEmail").addClass("show-error");
+					console.log('Email is empty');
+					valid = false;
+				} else if ( !pattern.test(email)) {
+						$("#errorEmail").addClass("show-error");
+						console.log('Email is INVALID');
+					valid = false;
+				} else {
+					console.log('Email is VALID');
+					$(".show-error").removeClass('show-error');
+				}
+
+
+
+				}
+
+
+
+
+
+
+}
+								   
+
+
+}());
